58e2b0b93502494c26e2d0de47caaed1e6954086
really-lilly <tatkalillian@gmail.com>
2021-08-24 12:58:57 -0700

    function to analyze single model

--- isMassConserved.py
+++ isMassConserved.py
@@ -66,3 +66,43 @@ def isMassConserved(source_dir, true_dir, false_dir):
                 f.close()
 
 
+def isMassConserved_single(ant):
+    #TODO: finish this
+    ant = ant.splitlines()
+    for line in ant:
+        if '->' in line and not line.startswith('#'):
+            line = line.replace(' ', '')  # strip spaces
+            # separate products and reactants by splitting at ->
+            reaction = line.split('->')
+            reactants = reaction[0]
+            # Remove the rate constant part
+            products = reaction[1].split(';')[0]
+            rxnType = ''
+            # Get reaction type by number of reactants and products
+            if '+' in reactants:
+                rxnType += 'bi-'
+            else:
+                rxnType += 'uni-'
+            # Now we do the same thing for the product half
+            if '+' in products:
+                rxnType += 'bi'
+            else:
+                rxnType += 'uni'
+
+            if rxnType == 'uni-bi':
+                # Separate products (reactants is already just one item
+                products = products.split('+')
+                if reactants in products:
+                    # mass is not conserved, write to dir
+                    return False
+            elif rxnType == 'bi-uni':
+                # products is already one item, need to split reactants
+                reactants = reactants.split('+')
+                if products in reactants:
+                    # mass is not conserved, write to dir
+                    return False
+            else:  # If the reaction type is uni-uni or bi-bi, we ignore and move to next reaction
+                continue
+        else:  # if the line is not a reaction, move to the next line
+            continue
+    return True
