cdc51ad4ac49320b89d6df4525e09c5c230643ff
really-lilly <tatkalillian@gmail.com>
2021-08-30 13:04:12 -0700

    add non essential reaction processing to zippedruns module

--- analyzeZippedRuns.py
+++ analyzeZippedRuns.py
@@ -4,7 +4,7 @@ import json
 import math
 import sys
 import zipfile
-
+import os
 import matplotlib
 import matplotlib.pyplot as plt
 import tellurium as te
@@ -149,6 +149,43 @@ def countEig(eig):
         if abs(eigenvalue.imag) > 0.1:
             numConjugates += 1
     return numConjugates
+
+def stringToList(indices):
+    indices = indices.split(', ')
+    indices[0] = indices[0][1:]
+    if len(indices) > 1:
+        indices[-1] = indices[-1][:-2]
+    for i in range(len(indices)):
+        indices[i] = int(indices[i])
+    return indices
+
+
+def makeNonEssRxnDict(modelPath, savePath):
+    # e.g. file = './data.json'
+    modelPath = 'C:\\Users\\tatka\\Desktop\\Models\\OSCILLATOR'
+    indices_dict = {}
+    for filename in os.listdir(modelPath):
+        os.chdir(modelPath)
+        if filename.startswith('Model'):
+            modelPath = os.path.join(modelPath, filename)
+            os.chdir(modelPath)
+            for file in os.listdir(modelPath):
+                if file.endswith('summary.txt'):
+                    with open(file, "r") as f:
+                        lines = f.readlines()
+                        f.close()
+                    model = lines[0].split(' = ')[1][:-5]
+                    indices = lines[4].split(' = ')[1]
+                    indices = stringToList(indices)
+                    indices_dict[model] = indices
+    with open(os.path.join(modelPath, 'oscillators_dict.json'), 'w') as f:
+        json.dump(indices_dict, f)
+
+
+def loadNonEssRxnDict(path):
+    with open(path, 'r') as f:
+        idx_dict = json.load(f)
+    return idx_dict
 
 print ("Type quit to exit and help to get help")
 print ("Commands: help, run, ant, stats, plotbest, snapshot, count, list, fitness, evolution, allfitness, quit")
