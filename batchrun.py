69350d880fba7a6ed917f62412a3da90cee6e090
really-lilly <tatkalillian@gmail.com>
2021-08-30 17:35:01 -0700

    add command line args to batchrun

--- batchrun.py
+++ batchrun.py
@@ -12,27 +12,33 @@ import time
 from datetime import date
 from datetime import datetime
 
-numberOfRuns = 10 # default
+import argparse
 
-argv = sys.argv[1:]
-options, args = getopt.getopt(argv, 'r:', [])
-for opt, arg in options: 
-    if opt in ('-r', ''):
-       numberOfRuns = int (arg)
-       
-print ("Batch set up for " + str (numberOfRuns) + " runs")
+parser = argparse.ArgumentParser()
+parser.add_argument("--runs", default=10, type=int, help="Change the number of evolution trials")
+parser.add_argument("--newConfigFile", default=None, type=str, help="Use a different configuration file")
+parser.add_argument("--numGenerations", default=None, type=int, help="Set the max number of generations")
+args = parser.parse_args()
+
+command = 'python ./evolve.py'
+if args.newConfigFile:
+    command += f' --newConfigFile {args.newConfigFile}'
+if args.numGenerations:
+    command += f' --numGenerations {str(args.numGenerations)}'
+
+print ("Batch set up for " + str(args.runs) + " runs")
 
 today = date.today()
 now = datetime.now()
 
-print ("Run Started on: ", today.strftime("%b-%d-%Y"))
-print ("at time: ", now.strftime("%H:%M:%S"), '\n')
+print("Run Started on: ", today.strftime("%b-%d-%Y"))
+print("at time: ", now.strftime("%H:%M:%S"), '\n')
 
 start = time.time()
-for i in range (numberOfRuns):
-    print ("-----------------------------------------------------")
-    print (" --- BATCH NUMBER --- " + str (i+1) + ' out of ' + str (numberOfRuns) + ' total.')
-    print ("-----------------------------------------------------")   
-    os.system('python ./evolve.py' + ' -g 500')
+for i in range(args.runs):
+    print("-----------------------------------------------------")
+    print(" --- BATCH NUMBER --- " + str(i+1) + ' out of ' + str(args.runs) + ' total.')
+    print("-----------------------------------------------------")
+    os.system(command)
 
-print ("Time taken to do batch runs = ", time.time() - start)
+print("Time taken to do batch runs = ", time.time() - start)
