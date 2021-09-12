5e373b4431b1d70ecf7792ae6929b45241672508
really-lilly <tatkalillian@gmail.com>
2021-09-12 11:10:57 -0700

    add multiprocessing

--- batchrun.py
+++ batchrun.py
@@ -9,8 +9,14 @@ import os, sys, getopt
 import time
 from datetime import date
 from datetime import datetime
+import multiprocessing
 
 numberOfRuns = 10 # default
+
+def runEvolution():
+    os.system('python evolve.py' + ' -g 500')
+
+
 
 argv = sys.argv[1:]
 options, args = getopt.getopt(argv, 'r:', [])
@@ -26,11 +32,23 @@ now = datetime.now()
 print ("Run Started on: ", today.strftime("%b-%d-%Y"))
 print ("at time: ", now.strftime("%H:%M:%S"), '\n')
 
+
 start = time.time()
-for i in range (numberOfRuns):
-    print ("-----------------------------------------------------")
-    print (" --- BATCH NUMBER --- " + str (i+1) + ' out of ' + str (numberOfRuns) + ' total.')
-    print ("-----------------------------------------------------")   
-    os.system('python .\evolve.py' + ' -g 500')
+
+processes = []
+for i in range(numberOfRuns):
+    p = multiprocessing.Process(target=runEvolution)
+    processes.append(p)
+    p.start()
+for process in processes:
+    process.join()
+
 
 print ("Time taken to do batch runs = ", time.time() - start)
+# for i in range (numberOfRuns):
+#     print ("-----------------------------------------------------")
+#     print (" --- BATCH NUMBER --- " + str (i+1) + ' out of ' + str (numberOfRuns) + ' total.')
+#     print ("-----------------------------------------------------")
+#     os.system('python evolve.py' + ' -g 500')
+#
+# print ("Time taken to do batch runs = ", time.time() - start)
